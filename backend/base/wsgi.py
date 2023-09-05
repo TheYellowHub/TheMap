c90327986cdae183671f218ed44ac2b78adcd459
Hana <hana.benami@gmail.com>
2023-09-05 21:33:01 +0300

    EnvKey

--- backend/base/wsgi.py
+++ backend/base/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+import envkey
 import os
 
 from django.core.wsgi import get_wsgi_application
--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import envkey
 import os
 import sys
 
@@ -8,7 +9,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line      
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
