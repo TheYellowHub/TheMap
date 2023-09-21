d0e49d7f368075d1d6d0acd6a3734d68f54e93bd
xiushak <30637265+xiushak@users.noreply.github.com>
2023-09-21 19:55:01 -0400

    map-2 basic frame of small doctor card

--- frontend/src/components/doctors/doctors/DoctorSmallCard.tsx
+++ frontend/src/components/doctors/doctors/DoctorSmallCard.tsx
@@ -6,9 +6,24 @@ interface DoctorSmallCardProps {
 }
 
 function DoctorSmallCard({ doctor, onClick }: DoctorSmallCardProps) {
+    const image = doctor.image
+        ? doctor.image.name
+        : "images/default-doctor-f.png";
+    const address = doctor.locations[0] ? doctor.locations[0].address ? doctor.locations[0].address : "no address" : "no location";
+    // TODO: replace with the real fields
+    const averageRating = 4.5;
+    const totalReviews = 10;
     return (
         <div className="doctorSmallCard" onClick={onClick}>
-            {doctor.fullName} {doctor.categories}
+            <img src={image} alt={doctor.fullName} />
+            <div className="doctorSmallCardData">
+                <div><h5>{doctor.fullName} </h5></div>
+                <div> {doctor.categories}</div>
+                <div> {address}</div>
+                <div>
+                    {averageRating} {totalReviews}
+                </div>
+            </div>
         </div>
     );
 }
