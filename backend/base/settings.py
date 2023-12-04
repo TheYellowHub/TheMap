4e5d8d205d954b0409fd543c26ab57b9d756598b
Hana <hana.benami@gmail.com>
2023-12-04 10:21:42 +0200

    allowed hosts

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(
-    ","
-)
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
