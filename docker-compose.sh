7ee2f6f1abe2e587a5fa99a919e58f1598b45b2f
Hana <hana.benami@gmail.com>
2023-12-04 18:56:43 +0200

    Lightsail WIP

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,4 +4,4 @@ DOCKER_COMPOSE_FILE=docker-compose-$ENV_NAME.yaml
 sudo docker-compose -f $DOCKER_COMPOSE_FILE down
 # sudo docker volume rm themap_postgres_data -f # Use this when you want to erase the entire database
 sudo docker-compose -f $DOCKER_COMPOSE_FILE build --no-cache
-sudo docker-compose -f $DOCKER_COMPOSE_FILE up
+sudo docker-compose -f $DOCKER_COMPOSE_FILE up --remove-orphans
