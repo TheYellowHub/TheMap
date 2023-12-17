a510c2a8c09b6e455eb568c706f69f8eac4beac0
Hana Oliver <hana.benami@gmail.com>
2023-12-17 23:23:12 +0200

    log errors in GA

--- frontend/src/utils/log.ts
+++ frontend/src/utils/log.ts
@@ -1,5 +1,12 @@
+import ReactGA from "react-ga4";
+
 export default function logError(error: Error) {
     if (process.env.NODE_ENV === "development") {
-        console.log(error);
+        console.log();
+    } else if (process.env.NODE_ENV === "production") {
+        ReactGA.event({
+            category: "Errors",
+            action: String(error),
+        });
     }
 }
