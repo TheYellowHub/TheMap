6bd53528908aeefc3d3d003a7284e6b39f4e06ce
Hana Oliver <hana.benami@gmail.com>
2023-12-23 23:14:47 +0200

    Fix & Delete account

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
+from . import auth0
+
 
 class User(models.Model):
     """
@@ -22,7 +24,7 @@ class User(models.Model):
         return str(self) == str(__value)
         
     def delete_remote_user(self):
-        print("delete_remote_user...")
+        auth0.delete(self)
 
 
 @receiver(pre_delete, sender=User)
