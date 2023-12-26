89258e95b6ddd749d5b460c95ae6e94e8c01853d
Hana Oliver <hana.benami@gmail.com>
2023-12-26 20:52:03 +0200

    missing navigation link

--- frontend/src/components/doctors/doctors/DoctorLocationDistance.tsx
+++ frontend/src/components/doctors/doctors/DoctorLocationDistance.tsx
@@ -7,43 +7,39 @@ import Icon from "../../utils/Icon";
 
 interface DoctorLocationDistanceProps {
     doctorLocation: DoctorLocation;
-    locationForDistanceCalculation: Location;
-    distanceUnit: DistanceUnit;
+    locationForDistanceCalculation?: Location;
+    distanceUnit?: DistanceUnit;
 }
 
 function DoctorLocationDistance({
-    doctorLocation: doctorLocation,
+    doctorLocation,
     locationForDistanceCalculation,
-    distanceUnit,
+    distanceUnit
 }: DoctorLocationDistanceProps) {
-    const distance = getDoctorLocationDistance(doctorLocation, locationForDistanceCalculation, distanceUnit);
+    const distance = locationForDistanceCalculation && getDoctorLocationDistance(doctorLocation, locationForDistanceCalculation, distanceUnit);
 
     const tooltipMessage = doctorLocation?.address ? <></> : <Tooltip>No link to address</Tooltip>;
 
     return (
-        <>
-            {distance && distance !== Infinity && (
-                <OverlayTrigger placement="bottom" overlay={tooltipMessage}>
-                    <p className="med-grey p-0 m-0">
-                        <a
-                            href={`${
-                                doctorLocation?.address
-                                    ? "http://maps.google.com/maps/dir/?api=1&dir_action=navigate&destination=" +
-                                    doctorLocation.address
-                                    : "#"
-                            }`}
-                            target="_blank"
-                            rel="noreferrer"
-                            onClick={(e) => e.stopPropagation()}
-                            className="a-only-hover-decoration"
-                        >
-                                {distance.toFixed(1)} {distanceUnit}
-                                <Icon icon="fas fa-location-arrow ps-1" solid={false} padding={false} />
-                        </a>
-                    </p>
-                </OverlayTrigger>
-            )}
-        </>
+        <OverlayTrigger placement="bottom" overlay={tooltipMessage}>
+            <p className="med-grey p-0 m-0">
+                <a
+                    href={`${
+                        doctorLocation?.address
+                            ? "http://maps.google.com/maps/dir/?api=1&dir_action=navigate&destination=" +
+                            doctorLocation.address
+                            : "#"
+                    }`}
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                    className="a-only-hover-decoration"
+                >
+                        {distance && distance !== Infinity && <>{distance.toFixed(1)} {distanceUnit}</>}
+                        <Icon icon="fas fa-location-arrow ps-1" solid={false} padding={false} />
+                </a>
+            </p>
+        </OverlayTrigger>
     );
 }
 
