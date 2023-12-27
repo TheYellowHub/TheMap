6a38bfaa08d9f191aff47268b0e5c49e22483da8
Hana Oliver <hana.benami@gmail.com>
2023-12-27 14:09:08 +0200

    Logout - redirect to main page

--- frontend/src/auth/AuthProviderImpl.tsx
+++ frontend/src/auth/AuthProviderImpl.tsx
@@ -5,6 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import AuthContext from "./AuthContext";
 import { AuthState } from "./authState";
 import { getCurrentUrl } from "../utils/utils";
+import { mainMapUrl } from "../AppRouter";
 
 export default function AuthProvider({ children }: React.PropsWithChildren) {
     const {
@@ -29,10 +30,7 @@ export default function AuthProvider({ children }: React.PropsWithChildren) {
     }, [auth0login]);
 
     const logout = useCallback(async () => {
-        await auth0logout({
-            logoutParams: { returnTo: getCurrentUrl() },
-
-        });
+        await auth0logout();
     }, [auth0logout]);
 
     const checkIfAdmin = useCallback(async () => {
