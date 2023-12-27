43cffc4013fead7dee83dcbd16536a923b5c6bfc
Hana Oliver <hana.benami@gmail.com>
2023-12-27 22:31:54 +0200

    NN and ICB tooltips

--- frontend/src/components/doctors/doctors/DoctorVerification.tsx
+++ frontend/src/components/doctors/doctors/DoctorVerification.tsx
@@ -1,6 +1,5 @@
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
-
 import { Doctor } from "../../../types/doctors/doctor";
+import Tooltip from "../../utils/Tooltip";
 
 interface DoctorVerificationProps {
     doctor: Doctor;
@@ -10,16 +9,16 @@ function DoctorVerification({ doctor }: DoctorVerificationProps) {
     return (
         <span className="verification text-nowrap">
             {doctor.nancysNook && (
-                <OverlayTrigger placement="bottom" overlay={<Tooltip>Nancy&apos;s Nook</Tooltip>}>
+                <Tooltip text="Nancy&apos;s Nook">
                     <img className="px-1" src="/images/NN.svg" alt="Nancy's Nook" />
-                </OverlayTrigger>
+                </Tooltip>
             )}
             {doctor.iCareBetter && (
-                <OverlayTrigger placement="bottom" overlay={<Tooltip>iCareBetter.com</Tooltip>}>
+                <Tooltip text="iCareBetter.com">
                     <a href={doctor.iCareBetter} target="_blank" rel="noreferrer" onClick={(e) => e.stopPropagation()}>
                         <img className="px-1" src="/images/iCareBetter.svg" alt="iCareBetter.com" />
                     </a>
-                </OverlayTrigger>
+                </Tooltip>
             )}
         </span>
     );
